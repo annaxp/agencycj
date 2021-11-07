4f102295082a1703d8d1759e12049948fb8a8113
Andrey Pevin <apevin@altenar.net>
2021-11-08 01:41:35 +0300

    m

---
+++ src/assets/js/form.js
@@ -0,0 +1,39 @@
+const formSend = () => {
+  document.addEventListener('DOMContentLoaded', function () {
+    const formWrapper = document.querySelector('.form-container')
+    getForm(formWrapper)
+  })
+
+  function getForm(wrapper) {
+    const form = wrapper.querySelector('form')
+    const name = form.querySelector('input[name="name"]')
+    const phone = form.querySelector('input[name="phone"]')
+    const button = form.querySelector('button')
+    button.onclick = (e) => {
+      e.preventDefault()
+      const data = `name=${name.value}&phone=${phone.value}`
+      const url = `${form.action}?${data}`
+      ;(async function () {
+        const result = await fetch(url).then((res) => res.data)
+        if (result) {
+          getSuccess(wrapper)
+        } else {
+          onError()
+        }
+      })()
+    }
+  }
+  function getSuccess(wrapper) {
+    wrapper.innerHTML = `
+      <div class="form-success">
+        <div class="form-success__content">${wrapper.getAttribute(
+          'data-success',
+        )}</div>
+      </div>
+    `
+  }
+  function onError() {
+    console.log('error')
+  }
+}
+export default formSend
