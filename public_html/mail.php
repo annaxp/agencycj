9a98b79d51ea33a80ebadf8f2c50a1c56bbb469b
Andrey Pevin <apevin@altenar.net>
2021-11-10 00:01:11 +0300

    form simple validation

--- public_html/mail.php
+++ public_html/mail.php
@@ -3,21 +3,27 @@ $botId = "2111200320:AAGuezHGXwnsBGtw6w8LE51SI1z1QZ9Rbsg";
 $chatId = "-701885969";
 $name = $_GET["name"];
 $phone = $_GET["phone"];
-$domen = ($_SERVER["HTTPS"] ? "https://" : "http://").$_SERVER["HTTP_HOST"];
-$message = "<b>Заявка с {$domen}</b>\n<b>Имя</b>: {$name}\n<b>Телефон</b>: {$phone}";
+$antispam = $_GET["antispam"];
+if (!$antispam) {
+  echo json_encode(["ok" => false]);
+} else {
+  $domen = ($_SERVER["HTTPS"] ? "https://" : "http://").$_SERVER["HTTP_HOST"];
+  $message = "<b>Заявка с {$domen}</b>\n<b>Имя</b>: {$name}\n<b>Телефон</b>: {$phone}";
+  
+  $data = array(
+    "chat_id" => $chatId,
+    "parse_mode" => "html",
+    "text" => $message
+  );
+  $url = "https://api.telegram.org/bot".$botId."/sendMessage";
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
+   
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  $html = curl_exec($ch);
+  curl_close($ch);
+}
 
-$data = array(
-  "chat_id" => $chatId,
-  "parse_mode" => "html",
-  "text" => $message
-);
-$url = "https://api.telegram.org/bot".$botId."/sendMessage";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
- 
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_HEADER, false);
-$html = curl_exec($ch);
-curl_close($ch);
 ?>
--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,30 +1,118 @@
 const formSend = () => {
+  const formState = {
+    hasErrors: [],
+    firstClick: true,
+    setHasErrors: ({ type, prop }) => {
+      if (type === true) {
+        if (!formState.hasErrors.includes(prop)) {
+          formState.hasErrors.push(prop)
+        }
+      } else {
+        formState.hasErrors = formState.hasErrors.filter(
+          (stateProp) => stateProp !== prop,
+        )
+      }
+      formState.listener &&
+        formState.listener({
+          hasErrors: formState.hasErrors,
+          firstClick: formState.firstClick,
+        })
+    },
+  }
+
   document.addEventListener('DOMContentLoaded', function () {
-    const formWrapper = document.querySelector('.form-container');
-    getForm(formWrapper);
-  });
+    const formWrapper = document.querySelector('.form-container')
+    getForm(formWrapper)
+  })
 
   function getForm(wrapper) {
-    const form = wrapper.querySelector('form');
-    const name = form.querySelector('input[name="name"]');
-    const phone = form.querySelector('input[name="phone"]');
-    const button = form.querySelector('button');
+    const form = wrapper.querySelector('form')
+    const name = form.querySelector('input[name="name"]')
+    const phone = form.querySelector('input[name="phone"]')
+    const button = form.querySelector('button')
+
     button.onclick = (e) => {
-      e.preventDefault();
-      const data = `name=${name.value}&phone=${phone.value}`;
-      const url = `${form.action}?${data}`;
+      e.preventDefault()
+      if (formState.hasErrors.length && formState.firstClick) {
+        formState.firstClick = false
+        setButtonDisabled(true)
+        return false
+      } else {
+      }
+
+      const data = `name=${name.value}&phone=${phone.value}&antispam=antispam`
+      const url = `${form.action}?${data}`
 
       fetch(url)
         .then((res) => res.json())
         .then((res) => onSuccess(res))
-        .catch((error) => console.log(error));
-    };
+        .catch((error) => console.log(error))
+    }
+
+    formState.listener = ({ hasErrors, firstClick }) => {
+      if (!hasErrors.length && !firstClick) {
+        setButtonDisabled(false)
+      } else if (hasErrors.length && !firstClick) {
+        setButtonDisabled(true)
+      }
+    }
+
+    checkName({ target: name })
+    checkPhone({ target: phone })
+
+    const events = ['onchange', 'onkeyup', 'onBlur']
+    events.forEach((event) => {
+      name[event] = checkName
+      phone[event] = checkPhone
+    })
 
     function onSuccess(result) {
       if (result?.ok) {
-        getSuccessHtml(wrapper);
+        getSuccessHtml(wrapper)
       } else {
-        onError(result);
+        onError(result)
+      }
+    }
+
+    function checkName({ target }) {
+      const value = target.value
+      const prop = 'name'
+      if (!value) {
+        setInputClass({ type: 'empty', target })
+        formState.setHasErrors({ type: true, prop })
+      } else {
+        setInputClass({ type: 'success', target })
+        formState.setHasErrors({ type: false, prop })
+      }
+    }
+
+    function checkPhone({ target }) {
+      const value = target.value
+      const prop = 'phone'
+      if (!value) {
+        setInputClass({ type: 'empty', target })
+        formState.setHasErrors({ type: true, prop })
+      } else {
+        setInputClass({ type: 'success', target })
+        formState.setHasErrors({ type: false, prop })
+      }
+    }
+
+    function setInputClass({ target, type }) {
+      if (type === 'empty') {
+        target.parentElement.classList.add('empty')
+      } else if (type === 'success') {
+        target.parentElement.classList.remove('empty')
+      }
+    }
+
+    function setButtonDisabled(disabled = true) {
+      if (disabled) {
+        form.classList.add('error')
+        button.setAttribute('disabled', true)
+      } else {
+        form.classList.remove('error')
+        button.removeAttribute('disabled')
       }
     }
 
@@ -35,12 +123,29 @@ const formSend = () => {
             wrapper.getAttribute('data-success') || 'Успех'
           }</div>
         </div>
-      `;
+      `
     }
 
     function onError(result) {
-      console.log(result);
+      console.log(result)
     }
+
+    // function setInputFilter(textbox, inputFilter) {
+    //   ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    //     textbox.addEventListener(event, function() {
+    //       if (inputFilter(this.value)) {
+    //         this.oldValue = this.value;
+    //         this.oldSelectionStart = this.selectionStart;
+    //         this.oldSelectionEnd = this.selectionEnd;
+    //       } else if (this.hasOwnProperty("oldValue")) {
+    //         this.value = this.oldValue;
+    //         this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+    //       } else {
+    //         this.value = "";
+    //       }
+    //     });
+    //   });
+    // }
   }
-};
-export default formSend;
+}
+export default formSend
